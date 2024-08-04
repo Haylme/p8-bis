da9b218163f074aad7b759aa2512539354aca545
nicolas girard <ngirard44@yahoo.fr>
2024-08-04 15:04:52 +0200

    finished Detail and DetailViewModel

--- app/src/main/java/com/example/projet8bis/ui/detail/Detail.kt
+++ app/src/main/java/com/example/projet8bis/ui/detail/Detail.kt
@@ -1,33 +1,440 @@
 package com.example.projet8bis.ui.detail
 
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.projet8bis.model.SimpleResponse
+import com.example.projet8bis.ui.edit.getDrawableUri
 import com.example.techradar.R
+import com.example.techradar.databinding.FragmentDetailBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import kotlin.properties.Delegates
+
 
 @AndroidEntryPoint
-class Detail : Fragment() {
+class Detail : Fragment(R.layout.fragment_detail) {
 
-    companion object {
-        fun newInstance() = Detail()
-    }
+    private var _binding: FragmentDetailBinding? = null
+
+    private val binding get() = _binding!!
+
+
+    private var id: Long = 0L
+
+
+    private var favorite by Delegates.notNull<Boolean>()
+
 
     private val viewModel: DetailViewModel by viewModels()
+
+
+    private lateinit var phoneValue: String
+
+    private lateinit var emailValue: String
+
+    private lateinit var birthday: String
+
+    private var wage by Delegates.notNull<Double>()
+
+
+    private lateinit var pictureUri: String
+
+    private lateinit var noteText: String
+    private lateinit var nameText: String
+    private lateinit var firstnameText: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO: Use the ViewModel
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
+
+
+        (activity as? AppCompatActivity)?.setSupportActionBar(binding.topAppbar)
+
+        (activity as? AppCompatActivity)?.supportActionBar?.title = ""
+
+        return binding.root
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
+        id = arguments?.getLong("id") ?: 0L
+
+
+
+
+        favorite = arguments?.getBoolean("favorite") ?: false
+
+        nameText = arguments?.getString("name") ?: ""
+
+        firstnameText = arguments?.getString("firstname") ?: ""
+
+        noteText = arguments?.getString("note") ?: ""
+
+        birthday = arguments?.getString("birthday").toString()
+
+        wage = arguments?.getDouble("wage") ?: 0.00
+
+        phoneValue = arguments?.getString("phone").toString()
+        emailValue = arguments?.getString(("email")).toString()
+
+        pictureUri = arguments?.getString("picture").toString()
+        bindAvatar(binding.avatar, pictureUri)
+
+
+        val avatar = binding.avatar
+        val callButton = binding.call
+        val smsButton = binding.message
+        val mailButton = binding.mailButton
+        val about = binding.about
+        val wageText = binding.wage
+        val note = binding.noteEditText
+        val name = binding.name
+        val firstname = binding.firstname
+
+        val currency = binding.wageTranslate
+
+
+        val backBar = binding.back
+
+        name.text = nameText
+
+        firstname.text = firstnameText
+
+        note.text = noteText
+
+        backBar.setOnClickListener {
+
+            findNavController().navigate(R.id.action_detail_to_home)
+
+        }
+
+
+        val formattedWage = if (wage % 1 == 0.0) {
+            String.format("%.0f", wage)
+        } else {
+            String.format("%.2f", wage)
+        }
+        wageText.text = getString(R.string.wage_text, formattedWage, getString(R.string.euro))
+
+
+
+
+
+        if (birthday.isNotEmpty()) {
+            try {
+                val sdf = SimpleDateFormat("dd/MM/yyyy")
+                val date = sdf.parse(birthday)
+                val currentDate = Calendar.getInstance()
+
+                date?.let {
+                    val birthdayCalendar = Calendar.getInstance().apply { time = it }
+                    var age = currentDate.get(Calendar.YEAR) - birthdayCalendar.get(Calendar.YEAR)
+                    if (currentDate.get(Calendar.DAY_OF_YEAR) < birthdayCalendar.get(Calendar.DAY_OF_YEAR)) {
+                        age--
+                    }
+                    about.text =
+                        getString(R.string.about_text, birthday, age, getString(R.string.ans))
+                }
+            } catch (e: Exception) {
+                return
+            }
+        } else {
+            return
+        }
+
+
+        val nameSize = name.text.length
+        val firstnameSize = firstname.text.length
+
+        if (nameSize + firstnameSize > 6) {
+
+            name.textSize = 16f
+            firstname.textSize = 16f
+
+        }
+
+
+        callButton.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneValue"))
+            startActivity(intent)
+
+        }
+
+        smsButton.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:$phoneValue"))
+            startActivity(intent)
+
+        }
+
+        mailButton.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto: $emailValue"))
+            startActivity(intent)
+
+        }
+
+
+
+
+        parentFragmentManager.setFragmentResultListener("editResult", this) { _, bundle ->
+            id = bundle.getLong("id")
+            nameText = bundle.getString("name") ?: ""
+            firstnameText = bundle.getString("firstname") ?: ""
+            phoneValue = bundle.getString("phone") ?: ""
+            emailValue = bundle.getString("email") ?: ""
+            wage = bundle.getDouble("wage")
+            noteText = bundle.getString("note") ?: ""
+            pictureUri = bundle.getString("pics").toString()
+            birthday = bundle.getString("birthday") ?: ""
+            favorite = bundle.getBoolean("favorite")
+
+
+            name.text = nameText
+            firstname.text = firstnameText
+
+
+            val formattedWage = if (wage % 1 == 0.0) {
+                String.format("%.0f", wage)
+            } else {
+                String.format("%.2f", wage)
+            }
+            wageText.text = getString(R.string.wage_text, formattedWage, getString(R.string.euro))
+
+            binding.noteEditText.text = noteText
+
+            binding.about.text = birthday
+
+            bindAvatar(avatar, pictureUri)
+
+
+
+
+
+            if (birthday.isNotEmpty()) {
+                try {
+                    val sdf = SimpleDateFormat("dd/MM/yyyy")
+                    val date = sdf.parse(birthday)
+                    val currentDate = Calendar.getInstance()
+
+                    date?.let {
+                        val birthdayCalendar = Calendar.getInstance().apply { time = it }
+                        var age =
+                            currentDate.get(Calendar.YEAR) - birthdayCalendar.get(Calendar.YEAR)
+                        if (currentDate.get(Calendar.DAY_OF_YEAR) < birthdayCalendar.get(Calendar.DAY_OF_YEAR)) {
+                            age--
+                        }
+                        about.text =
+                            getString(R.string.about_text, birthday, age, getString(R.string.ans))
+                    }
+                } catch (e: Exception) {
+                    return@setFragmentResultListener
+                }
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Aucune date de naissance renseignée",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+
+    }
+
+    @Deprecated(
+        "Deprecated in Java", ReplaceWith(
+            "inflater.inflate(R.menu.appbar_menu, menu)",
+            "com.example.techradar.R"
+        )
+    )
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+
+        inflater.inflate(R.menu.appbar_menu, menu)
+
+        val favItem = menu.findItem(R.id.fav)
+        updateIcon(favItem, favorite)
+
+
+        val suppItem = menu.findItem(R.id.supp)
+        val suppView = suppItem.actionView
+        suppView?.setOnLongClickListener {
+            Toast.makeText(requireContext(), getString(R.string.supprimer), Toast.LENGTH_SHORT)
+                .show()
+            true
+        }
+
+        val favView = favItem.actionView
+        favView?.setOnLongClickListener {
+            Toast.makeText(requireContext(), getString(R.string.favoris), Toast.LENGTH_LONG).show()
+            true
+
+        }
+        val editItem = menu.findItem(R.id.edit_button)
+        val editView = editItem.actionView
+        editView?.setOnLongClickListener {
+            Toast.makeText(requireContext(), getString(R.string.edit), Toast.LENGTH_LONG).show()
+            true
+
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        when (item.itemId) {
+            R.id.fav -> {
+
+                if (favorite) {
+                    val bool = false
+                    favorite = bool
+
+                    updateIcon(item, favorite)
+                    viewModel.updateData(id, favorite)
+
+                } else {
+                    val bool = true
+                    favorite = bool
+                    updateIcon(item, favorite)
+                    viewModel.updateData(id, favorite)
+
+                }
+
+
+            }
+
+            R.id.edit_button -> {
+                val bundle = Bundle().apply {
+                    putLong("id", id)
+                    putString("name", nameText)
+                    putString("firstname", firstnameText)
+                    putString("phone", phoneValue)
+                    putString("email", emailValue)
+                    putDouble("wage", wage)
+                    putString("note", noteText)
+                    putString("pics", pictureUri)
+                    putString("birthday", birthday)
+                    putBoolean("favorite", favorite)
+                }
+
+                findNavController().navigate(R.id.action_detail_to_edit, bundle)
+            }
+
+            R.id.supp -> {
+
+                val color = ContextCompat.getDrawable(binding.root.context, R.color.primary)
+
+                context?.let {
+                    MaterialAlertDialogBuilder(it)
+                        .setBackground(color)
+                        .setTitle(R.string.suppression)
+                        .setMessage(R.string.supprimer_candidat)
+                        .setNegativeButton(R.string.annuler) { dialog, _ ->
+                            dialog.cancel()
+                        }
+                        .setPositiveButton(R.string.confirmer) { dialog, _ ->
+                            dialog.apply {
+                                viewModel.deleteCandidate(id)
+
+                                findNavController().navigate(R.id.action_detail_to_home)
+                            }
+
+                        }
+                }
+                    ?.show()
+            }
+        }
+
+        return true
+    }
+
+
+    private fun updateIcon(menuItem: MenuItem, isFavorite: Boolean) {
+        if ((isFavorite)) {
+            menuItem.setIcon(R.drawable.baseline_star_24)
+
+        } else {
+
+            menuItem.setIcon(R.drawable.favorite_foreground)
+
+        }
+
+
+    }
+
+    private fun bindAvatar(avatar: ImageView, str: String) {
+
+
+        if (str.isNotEmpty()) {
+            val colourInt = ContextCompat.getColor(binding.root.context, R.color.transparent)
+
+            avatar.setBackgroundColor(colourInt)
+
+            val drawable =
+                getDrawableUri(binding.root.context, R.drawable.baseline_category_24).toString()
+
+            if (str == drawable) {
+
+                val colourNew = ContextCompat.getColor(binding.root.context, R.color.picture)
+                avatar.setBackgroundColor(colourNew)
+
+            }
+        } else {
+            val colourNew = ContextCompat.getColor(binding.root.context, R.color.picture)
+            avatar.setBackgroundColor(colourNew)
+            //avatar.setImageIcon(R.drawable.baseline_category_24)
+        }
+
+
+        val uri = Uri.parse((str))
+        Glide.with(avatar.context)
+            .load(uri)
+
+            .into(avatar)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/com/example/projet8bis/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/projet8bis/ui/detail/DetailViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.projet8bis.ui.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.projet8bis.data.DataRepository
+import com.example.projet8bis.model.Content
+import com.example.projet8bis.model.SimpleResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -11,4 +19,58 @@ class DetailViewModel @Inject constructor(
     private val dataRepository: DataRepository,
 
     ) : ViewModel() {
+
+
+    private val _detailError = MutableStateFlow<String?>(null)
+    val detailError: StateFlow<String?> = _detailError
+
+    private val _detailAdd = MutableStateFlow(SimpleResponse.initial<Content?>())
+    val detailAdd: StateFlow<SimpleResponse<Content?>> = _detailAdd.asStateFlow()
+
+
+    private val _updateUserFav = MutableStateFlow(SimpleResponse.initial<Boolean>())
+    val updateUserFav: StateFlow<SimpleResponse<Boolean>> = _updateUserFav.asStateFlow()
+
+    private val _translate = MutableStateFlow(SimpleResponse.initial<Double>())
+    val translate: StateFlow<SimpleResponse<Double>> = _translate.asStateFlow()
+
+    fun resetToast() {
+        _detailError.value = null
+
+    }
+
+    fun resetError() {
+
+        _detailAdd.value = SimpleResponse.initial()
+    }
+
+
+
+
+
+
+
+
+    fun updateData(id: Long, bool: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dataRepository.updateFav(id, bool)
+
+
+            _updateUserFav.value = SimpleResponse.success(bool)
+        }
+    }
+
+
+    fun deleteCandidate(id: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                dataRepository.suppressUser(id)
+                _detailAdd.value = SimpleResponse.success(null)
+            } catch (e: Exception) {
+                _detailAdd.value = SimpleResponse.failure(Exception("Unknown error"))
+                _detailError.value = "Unknown error"
+            }
+        }
+    }
+
 }
